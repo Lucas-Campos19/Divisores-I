ac23310c8cb4d24fd562cc9f5d906c8c29809799
Lucas <lucas.cgoncalves7@gmail.com>
2023-03-20 16:55:12 -0300

    Adicionar arquivos de projeto.

---
+++ Divisores I/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisores_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //Ler um número inteiro N e calcular todos os seus divisores.
+
+         // Entrada
+         // O arquivo de entrada contém um valor inteiro.
+            
+         // Saída
+         // Escreva todos os divisores positivos de N, um valor por linha.
+
+            int N = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= N; i++)
+            {
+                if(N % i == 0)
+                {
+                    Console.WriteLine(i);
+                   
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
